4e2895137c593c4bc2710507954bc19fb77802c8
Ray Deck <ray@raydeck.com>
2018-04-21 17:14:55 -0400

    Updating bridge

--- ios/rn-swift-bridge.m
+++ ios/rn-swift-bridge.m
@@ -3,5 +3,7 @@
 @interface RCT_EXTERN_MODULE(RNSRegistry, RCTEventEmitter)
 RCT_EXTERN_METHOD(setData:(NSString *)key data:(Any *)data success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
 RCT_EXTERN_METHOD(removeData:(NSString *)key success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(getData:(NSString *)key success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
 RCT_EXTERN_METHOD(addEvent:(NSString *)type key:(NSString *)key success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(removeEvent:(NSString *)key success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
 @end
