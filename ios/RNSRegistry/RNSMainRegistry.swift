019b86848ecf2a5df99e684b9136d612247b8e4d
Ray Deck <ray@raydeck.com>
2018-05-20 09:36:47 -0400

    Refactor to use new static model for RNSRegistry

--- ios/RNSRegistry/RNSMainRegistry.swift
+++ ios/RNSRegistry/RNSMainRegistry.swift
@@ -1,33 +1,60 @@
 import Foundation
 public typealias cbtype = (Any) -> Bool
+var events:[String: [String: cbtype]] = [:]
+var data:[String: Any] = [:]
+var q = DispatchQueue(label: "RNSQueue")
 open class RNSMainRegistry {
-    private  var events:[String: [String: cbtype]] = [:]
-    public  var data: [String: Any] = [:]
-    public static var main:RNSMainRegistry = RNSMainRegistry()
-    public func addEvent(type:String, key:String, callback:@escaping cbtype) -> String {
-        if events[type] == nil { events[type] = [:] }
-        events[type]![key] = callback
+    public class func addEvent(type: String, key: String, callback: @escaping cbtype) -> String {
+        q.sync() {
+            if events[type] == nil { events[type] = [:] }
+            events[type]?[key] = callback
+        }
         return key
     }
-    public func addEvent(type: String, callback: @escaping cbtype) -> String {
-        let key = UUID().uuidString
-        return addEvent(type:type, key: key, callback: callback)
-    }
-    public func removeEvent(type: String, key: String) {
-        events[type]?.removeValue(forKey: key)
-    }
-    func removeEvent(key: String) {
-        events.forEach() { (arg) in
-            self.removeEvent(type: arg.key, key: key)
+    public class func removeEvent(type: String, key: String) {
+        let _ = q.sync() {
+            events[type]?.removeValue(forKey: key)
         }
     }
-    public func triggerEvent(_ type: String, data: Any) -> Bool {
-        guard let es = events[type] else { return false }
-        var ret:Bool = true
-        es.forEach() { k, cb in
-            if !ret { return }
-            if !cb(data) { ret = false }
+    public class func removeEvent(key: String) {
+        let _ = q.sync() {
+            events.keys.forEach() { k in
+                events[k]?.removeValue(forKey: key)
+            }
         }
-        return ret
+    }
+    public class func removeEvents(type: String) {
+        let _ = q.sync() {
+            guard let e = events[type] else { return }
+            e.keys.forEach() { k in
+                events[type]?.removeValue(forKey: k)
+            }
+            events[type] = nil
+        }
+    }
+    public class func triggerEvent(type: String, data: Any) -> Bool {
+        return q.sync() {
+            guard let es = events[type] else { return false }
+            for thisKey:String in es.keys {
+                guard let cb = es[thisKey] else { continue }
+                if !cb(data) { return false }
+            }
+            return true
+        }
+    }
+    public class func setData(key: String, value: Any) {
+        let _ = q.sync() {
+            data[key] = value
+        }
+    }
+    public class func getData(key: String) -> Any? {
+        return q.sync() {
+            return data[key]
+        }
+    }
+    public class func removeData(key: String) {
+        let _ = q.sync() {
+            data.removeValue(forKey: key)
+        }
     }
 }
--- ios/RNSRegistry/RNSRegistry.swift
+++ ios/RNSRegistry/RNSRegistry.swift
@@ -4,25 +4,31 @@ class RNSRegistry: RCTEventEmitter {
     static var savedEventKeys:[String] = []
     override init() {
         super.init()
-        RNSRegistry.savedEventKeys.forEach() { k in
-            RNSMainRegistry.main.removeEvent(key: k)
+        if RNSRegistry.savedEventKeys.count > 0 {
+                RNSRegistry.savedEventKeys.forEach() { k in
+                RNSMainRegistry.removeEvent(key: k)
+            }
         }
         RNSRegistry.savedEventKeys = []
     }
     @objc func setData(_ key: String, data: Any?, success: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-        RNSMainRegistry.main.data[key] = data
+        if let d = data {
+            RNSMainRegistry.setData(key: key, value: d)
+        } else {
+            RNSMainRegistry.removeData(key: key)
+        }
         success(true)
     }
     @objc func removeData(_ key: String, success: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-        RNSMainRegistry.main.data.removeValue(forKey: key)
+        RNSMainRegistry.removeData(key: key)
         success(true)
     }
     @objc func getData(_ key: String, success: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-        guard let ret = RNSMainRegistry.main.data[key] else { reject("no_data", "No data at key " + key, nil); return }
+        guard let ret = RNSMainRegistry.getData(key: key) else { reject("no_data", "No data at key " + key, nil); return }
         success(ret)
     }
     @objc func addEvent(_ type: String, key: String, success: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-        let k = RNSMainRegistry.main.addEvent(type: type, key: key) { data in
+        let k = RNSMainRegistry.addEvent(type: type, key: key) { data in
             self.sendEvent(withName: "RNSRegistry", body: ["type": type, "key": key, "data": data]);
             return true
         }
@@ -30,13 +36,13 @@ class RNSRegistry: RCTEventEmitter {
         success(true)
     }
     @objc func removeEvent(_ key: String, success: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-        RNSMainRegistry.main.removeEvent(key: key)
+        RNSMainRegistry.removeEvent(key: key)
         success(true)
     }
     override func supportedEvents() -> [String]! {
         return ["RNSRegistry"]
     }
     override class func requiresMainQueueSetup() -> Bool {
-        return false;
+        return true;
     }
 }
