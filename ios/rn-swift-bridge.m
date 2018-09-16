bf57305491186b4a3a68242e8a5415b770f6026a
Ray Deck <ray@raydeck.com>
2018-09-16 09:13:53 -0400

    Added by RNSB

--- ios/rn-swift-bridge.m
+++ ios/rn-swift-bridge.m
@@ -2,6 +2,7 @@
 #import <React/RCTBridgeModule.h>
 @interface RCT_EXTERN_MODULE(RNSRegistry, RCTEventEmitter)
 RCT_EXTERN_METHOD(setData:(NSString *)key data:(Any *)data success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(saveData:(NSString *)key data:(Any *)data success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
 RCT_EXTERN_METHOD(removeData:(NSString *)key success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
 RCT_EXTERN_METHOD(getData:(NSString *)key success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
 RCT_EXTERN_METHOD(addEvent:(NSString *)type key:(NSString *)key success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
