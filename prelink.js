498ed9349b5b60d6601ee3ed92353077fa006bd4
Ray Deck <ray@raydeck.com>
2018-09-16 13:45:46 -0400

    Fixing prelink

--- prelink.js
+++ prelink.js
@@ -4,28 +4,40 @@ const fs = require("fs");
 const plist = require("plist");
 const glob = require("glob");
 const package = require(Path.join(process.cwd(), "package.json"));
-const { peerDependencies, startupClasses } = package;
-const newStartupClasses = [];
-if (peerDependences && typeof peerDependencies === "object") {
-  Object.keys(peerDependencies).forEach(node_module => {
+const { dependencies } = package;
+let newStartupClasses = [];
+const findNodeModule = (node_module, cwd) => {
+  if (!cwd) return;
+  const p = Path.join(cwd, "node_modules", node_module, "package.json");
+  if (fs.existsSync(p)) return p;
+  else return findNodeModule(Path.dirname(p));
+};
+if (dependencies && typeof dependencies === "object") {
+  Object.keys(dependencies).forEach(node_module => {
     try {
-      const { startupClasses } = require(require.resolve(
-        Path.join(node_module, "package.json")
-      ));
+      const path = findNodeModule(node_module, process.cwd());
+      const { startupClasses } = require(path);
       if (startupClasses) {
         newStartupClasses = newStartupClasses.concat(startupClasses);
       }
-    } catch (e) {}
+    } catch (e) {
+      console.log("Error", e);
+    }
   });
+
   //OK let's load up our plist now
   const pglobs = glob.sync(Path.join("ios", "*", "info.plist"));
   if (!pglobs) {
     console.log("Could not find the plist file");
     return;
   }
-  const pglob = pglobs.shift();
-  const txt = fs.readFileSync(pglob, { encoding: "UTF8" });
-  const o = plist.parse(txt);
-  o.RNSRClasses = newStartupClasses.filter(v => v);
-  fs.writeFileSync(pglob, plist.build(o));
+  pglobs.forEach(pglob => {
+    const txt = fs.readFileSync(pglob, { encoding: "UTF8" });
+    const o = plist.parse(txt);
+    o.RNSRClasses = newStartupClasses.filter(v => v);
+    fs.writeFileSync(pglob, plist.build(o));
+  });
+  console.log("Updated RNSR classes to", newStartupClasses.join(", "));
+} else {
+  console.log("No dependencies to work with");
 }
